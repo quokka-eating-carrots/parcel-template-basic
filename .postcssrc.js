ab7797ebc6b1cfcf1a4f99a6b35da600eb65b6f1
quokka-eating-carrots <ovzip7@gmail.com>
2022-10-12 14:35:02 +0900

    Create project

---
+++ .postcssrc.js
@@ -0,0 +1,13 @@
+// import autoprefixer form 'autoprefixer'
+const autoprefixer = require('autoprefixer')
+
+// export {
+//   plugins: [
+//     autoprefixer
+//   ]
+// }
+module.exports = {
+  plugins: [
+    autoprefixer
+  ]
+}
